46e8d6db3820f5447465f8c4ef6cb1ebf54cf3b2
Paul Lucas <plucas@hometrack.com>
2019-12-04 10:28:17 +0000

    move plot files to nhbc

--- app/process/nhbc/manage_plot_files.py
+++ app/process/nhbc/manage_plot_files.py
@@ -1,4 +1,5 @@
 from app.core.aws import s3
+from app.settings.envs import NHBC_BUCKET, NHBC_SOURCE_DIR
 import datetime
 import os
 import argparse
@@ -6,8 +7,6 @@ import argparse
 # Used as default if no month  is passed to the process
 current_month = datetime.datetime.now().strftime('%Y%m')
 
-DESTINATION_BUCKET = 'nhbc-eu-west-2-318016054559'
-SOURCE_FOLDER = 'S:\\HOMETRACK_ROOT\\Techdev\\Raw Data\\020_NHBCNewBuildDataset\\data'
 FILENAME_FMT = 'raw/{month}/{filename}'
 
 TRANSFORMED_FILE_PREFIX = 'transformed/plots/month={month}/part-00000-'
@@ -18,7 +17,7 @@ def upload_single_file(file_name, file_directory, month):
     """Uploads the given file/directory to the nhbc raw bucket for the given month."""
     full_path = os.path.join(file_directory, file_name)
     print(f'Uploading file {full_path}...')
-    s3.upload_file(full_path, DESTINATION_BUCKET, FILENAME_FMT.format(month=month, filename=file_name))
+    s3.upload_file(full_path, NHBC_BUCKET, FILENAME_FMT.format(month=month, filename=file_name))
 
 
 def upload_files(month):
@@ -26,7 +25,7 @@ def upload_files(month):
     Iterates over the particular months files and uploads them to s3.
     Where a month is not given, the current month will be used.
     """
-    source_directory = os.path.join(SOURCE_FOLDER, month)
+    source_directory = os.path.join(NHBC_SOURCE_DIR, month)
     for source_file in os.listdir(source_directory):
         upload_single_file(source_file, source_directory, month)
     print('Upload complete.')
@@ -35,7 +34,7 @@ def upload_files(month):
 def find_transformed_file(month):
     month_format = f'{month[:4]}-{month[-2:]}'
     found_files = s3.list_objects_v2(
-        Bucket=DESTINATION_BUCKET, Prefix=TRANSFORMED_FILE_PREFIX.format(month=month_format)
+        Bucket=NHBC_BUCKET, Prefix=TRANSFORMED_FILE_PREFIX.format(month=month_format)
     )['Contents']
     if len(found_files) == 1:
         return found_files[0]['Key']
@@ -47,8 +46,8 @@ def download_files(month):
     if not transformed_key:
         print('No transformed file found...')
         return
-    with open(os.path.join(SOURCE_FOLDER, month, TRANSFORMED_FILE_NAME), 'wb') as data:
-        s3.download_fileobj(DESTINATION_BUCKET, transformed_key, data)
+    with open(os.path.join(NHBC_SOURCE_DIR, month, TRANSFORMED_FILE_NAME), 'wb') as data:
+        s3.download_fileobj(NHBC_BUCKET, transformed_key, data)
 
 
 def process_action(state):
