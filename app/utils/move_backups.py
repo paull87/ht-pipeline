1f7856889c4c0377d541deb2854ce1f2690dbe83
Paul Lucas <plucas@hometrack.com>
2019-12-09 16:16:15 +0000

    Added option to get latest backup file

--- app/utils/move_backups.py
+++ app/utils/move_backups.py
@@ -2,12 +2,15 @@ import re
 import os
 from app.core.remote_windows import RemoteSession
 from app.settings import envs, secrets
+from app.core.logger import logger
 
 
 def copy_backups(remote_server, backup_regex, source_directory, target_directory):
-    files = remote_server.ls(envs.LON_SQL_04_DB_BACKUPS)
+    session = RemoteSession(remote_server, (secrets.USER_NAME, secrets.PASSWORD))
+    files = session.ls(source_directory)
     for file in find_backups(backup_regex, files):
-        remote_server.copy(
+        logger.info(f'Copying file {file} to {target_directory}')
+        session.copy(
             target=os.path.join(source_directory, file),
             destination=target_directory,
         )
@@ -17,6 +20,18 @@ def find_backups(file_regex, files):
     for file in files:
         if re.search(file_regex, file):
             yield file
+
+
+def get_latest_backup(server, source_directory, file_regex):
+    session = RemoteSession(server, (secrets.USER_NAME, secrets.PASSWORD))
+    backups = session.ls(source_directory)
+    version_backups = list(find_backups(file_regex, backups))
+    if len(version_backups) == 0:
+        logger.error(f'No backup found matching {file_regex}')
+        raise Exception(f'No backup found matching {file_regex}')
+    backup_file = version_backups[-1]
+    logger.info(f'Found backup {backup_file}')
+    return backup_file
 
 
 if __name__ == '__main__':
