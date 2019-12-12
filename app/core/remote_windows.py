1c2116c03fa483edb2b4c0c69109634d39ab3562
Paul Lucas <plucas@hometrack.com>
2019-12-12 11:47:53 +0000

    Added logging

--- app/core/remote_windows.py
+++ app/core/remote_windows.py
@@ -1,4 +1,5 @@
 import winrm
+import os
 from app.core.logger import logger
 
 
@@ -12,6 +13,7 @@ class RemoteSession:
         return winrm.Session(self._server, self._auth, transport='credssp')
 
     def run_cmd(self, command):
+        logger.info(f'running {command} against {self._server}')
         result = self._session.run_cmd(command)
         if result.std_err:
             logger.error(f'Error running {command} against {self._server}: {result.std_err.decode("utf-8")}')
@@ -20,21 +22,37 @@ class RemoteSession:
     def ls(self, directory=''):
         """Runs the equivalent ls or dir for a remote connection for the given directory."""
         cmd = f'DIR {directory} /B'
-        logger.info(f'running {cmd} against {self._server}')
-        result = self.run_cmd(f'DIR {directory} /B')
+        result = self.run_cmd(cmd)
         return split_files(result.std_out.decode('utf-8'))
 
     def copy(self, target, destination):
         """Copies the given target file to the given target file/directory"""
-        cmd = f'COPY {target} {destination}'
-        result = self.run_cmd(cmd)
-        return result.status_code
+        if not self._check_file_changed(destination, target):
+            cmd = f'COPY {target} {destination}'
+            result = self.run_cmd(cmd)
+            return result.status_code
+        else:
+            logger.info(f'File {destination} already exists and was not copied')
 
     def delete_file(self, file):
         """Deletes the given target file."""
         cmd = f'DEL /f {file}'
         result = self.run_cmd(cmd)
         return result.status_code
+
+    def file_modified_date(self, file):
+        """Returns the modified date of a file."""
+        return False
+        # TODO: forfiles doesn't support UNC so have to find another way to get file date.
+        directory, file_name = os.path.split(file)
+        cmd = f'forfiles /P {directory} /M {file_name} /C "cmd /c echo @FDATE @FTIME"'
+        result = self.run_cmd(cmd)
+        return result.std_out.decode('utf-8').strip()
+
+    def _check_file_changed(self, destination, target):
+        file_name = os.path.basename(destination)
+        target_file = os.path.join(target, file_name)
+        return self.file_modified_date(destination) == self.file_modified_date(target_file)
 
 
 def split_files(file_string):
